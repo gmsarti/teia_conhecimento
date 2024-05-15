2ce0fc88f37ef081484d93e953d8d0beb3a9fe0d
Gustavo Mescoki Sarti <gmsarti@gmail.com>
2024-05-15 18:30:38 -0300

    fix for running locally the  page

--- app/api.py
+++ app/api.py
@@ -1,21 +1,3 @@
-# from fastapi import FastAPI, Request
-# from fastapi.responses import HTMLResponse
-# from fastapi.templating import Jinja2Templates
-
-# app = FastAPI()
-# templates = Jinja2Templates(directory="templates")
-
-
-# # @app.get("/")
-# # async def root():
-# #     return {"race": "half-orc"}
-
-
-# @app.get("/", response_class=HTMLResponse)
-# async def root(request: Request):
-#     return templates.TemplateResponse("under_construction.html", {"request": request})
-
-
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
