fa4723736d42083a7361eb70a47a42bcc324cca0
Gustavo Mescoki Sarti <gmsarti@gmail.com>
2024-05-16 11:25:37 -0300

    new folder structure

--- app/controllers/api.py
+++ app/controllers/api.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from paths import TEMPLATES
+
 app = FastAPI()
-templates = Jinja2Templates(directory="app/templates")  # Updated directory
+templates = Jinja2Templates(directory=TEMPLATES)  # Updated directory
 
 
 @app.get("/", response_class=HTMLResponse)
---
+++ paths.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+ROOT = Path(__file__).parent
+VIEWS = ROOT / "app" / "views"
+TEMPLATES = VIEWS / "templates"
