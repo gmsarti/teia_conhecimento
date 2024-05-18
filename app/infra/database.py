949c41cc304e96e8eff4a0516447de2a71ac50b3
Gustavo Mescoki Sarti <gmsarti@gmail.com>
2024-05-18 11:33:06 -0300

    setting up environment variables

--- app/infra/database.py
+++ app/infra/database.py
@@ -1,11 +1,23 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
+
+load_dotenv()
 
 
 def query(qry: str):
     # Database connection details
+    db_host = os.environ.get("POSTGRES_HOST")
+    db_port = os.environ.get("POSTGRES_PORT")
+    db_name = os.environ.get("POSTGRES_DB")
+    db_user = os.environ.get("POSTGRES_USER")
+    db_password = os.environ.get("POSTGRES_PASSWORD")
+
     # database_url = "postgresql+psycopg2://your_username:your_password@localhost:5432/your_database_name"
     database_url = (
-        "postgresql+psycopg2://postgres:local_password@localhost:5432/postgres"
+        # "postgresql+psycopg2://postgres:local_password@localhost:5432/postgres"
+        f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
     )
 
     engine = create_engine(database_url)
